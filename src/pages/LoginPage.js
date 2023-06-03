b0273e83d7f3cc42a8a86b9d8d2b29769880338f
NikaMzhavanadze87 <n.m.mzhavanadze@gmail.com>
2023-06-03 20:28:05 +0400

    shualeduri-router

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -3,40 +3,72 @@ import { useNavigate } from 'react-router-dom';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
+import axios from 'axios';
+import { Password } from '@mui/icons-material';
 
 const LoginPage = () => {
-  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [error,setError] = useState('');
+  const [accesToken,setAccesToken] = useState('');
   const navigate = useNavigate();
 
-  const handleLogin = () => {
-    // Your login logic goes here
-    navigate('/'); // Redirect to the home page after successful login
+  
+  const handleLogin = async (e) => {
+    try {
+      e.preventDefault();
+      setError('')
+      if(!email || !password){
+        setError('please fill your information')
+        return
+      }
+      const response = await axios.post(
+        'https://accounts.tnet.ge/api/ka/user/auth',
+        {
+          Email: email,
+          Password: password,
+        }
+      );
+      const token = response.data.data.acces_token;
+      setAccesToken(token);
+      navigate('/products');
+
+    } catch (error) {
+      setError('your email or password is incorrect')
+    }
   };
 
   return (
     <Box sx={{ maxWidth: 300, margin: 'auto', marginTop: 1 }}>
       <h2>Login</h2>
-      <TextField
-        label="Username"
-        variant="outlined"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        fullWidth
-        margin="normal"
-      />
-      <TextField
-        label="Password"
-        variant="outlined"
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        fullWidth
-        margin="normal"
-      />
-      <Button variant="contained" onClick={handleLogin}>
-        Login
-      </Button>
+      <form onSubmit={handleLogin}>
+        <TextField
+          label="email"
+          variant="outlined"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          fullWidth
+          margin="normal"
+        />
+        
+        <TextField
+          label="password"
+          variant="outlined"
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          fullWidth
+          margin="normal"
+        />
+        <Button type="submit" variant="contained">
+          Login
+        </Button>
+        </form>
+        <br/>
+        <p style={{
+            color:'red'
+
+        }}>{error}</p>
     </Box>
   );
 };
